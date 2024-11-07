40edbbb6be00fee197f74c95eed699991ad6e902
pro <mint@pro.com>
2024-11-07 15:28:52 +0200

    Initial templates

---
+++ src/app/components/profit/profit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profit',
+  standalone: true,
+  imports: [],
+  templateUrl: './profit.component.html',
+  styleUrl: './profit.component.scss'
+})
+export class ProfitComponent {
+
+}
---
+++ src/app/components/stocks/stocks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stocks',
+  standalone: true,
+  imports: [],
+  templateUrl: './stocks.component.html',
+  styleUrl: './stocks.component.scss'
+})
+export class StocksComponent {
+
+}
